213b3396da6c3e747296beecb4d5d4b9fd5889aa
MehediHasan25 <ishanmehadi@gmail.com>
2019-04-26 18:07:50 +0600

    React crud api complete initial commit

---
+++ app.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+const mongoose= require('mongoose');
+const PersonModel = require('../rest-api/model/Person');
+const cors= require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+
+
+mongoose.connect('mongodb://localhost/Person',{ useNewUrlParser: true })
+ .then(()=> console.log('Connected to Mongodb'))
+ .catch(err=> console.error('Couldnot connect to Mongodb..', err));
+ 
+
+ app.get('/', (req,res)=>{
+    PersonModel.find((err, response)=>{
+        if(err){
+            console.log(err);
+        }
+        res.json(response );
+    });
+ });
+
+ app.get('/get-id/:id',(req,res)=>{
+    PersonModel.findById(req.params.id, (err,response)=>{
+        if(!err){
+            res.json(response);
+        }
+        console.log(err);
+    });
+ });
+
+ app.post('/add', (req,res)=>{
+    const newPerson = new PersonModel({
+        name:req.body.name,
+        organization: req.body.organization,
+        designation: req.body.designation
+    });
+
+        newPerson.save()
+        .then(person =>{
+            console.log(person);
+            res.json(person)
+        })
+        .catch(err=>{
+            res.json(err)
+        })
+ });
+
+
+
+ app.post('/update/:id',(req,res)=>{
+    PersonModel.findById(req.params.id, (err,response)=>{
+        if(err){
+            response.status(400).json({error:"Id is not found"});
+        }else{
+            response.name= req.body.name,
+            response.organization= req.body.organization,
+            response.designation= req.body.designation
+        
+        response.save()
+        .then(person =>{
+         res.status(200).json(person);
+        })
+        .catch(err=>{
+            res.status(400).json({error:"not updated"})
+        })
+        }
+    });
+ });
+
+
+
+app.delete('/delete/:id',(req,res)=>{
+    PersonModel.findByIdAndDelete({_id : req.params.id}, (error,response)=>{
+        if(error){
+            res.status(400).json("delete failed!");
+        }
+        res.status(200).json(response);
+    });
+});
+ 
+const port= process.env.PORT || 5000; 
+app.listen(port, ()=> console.log(`Server is running on ${port}`));
